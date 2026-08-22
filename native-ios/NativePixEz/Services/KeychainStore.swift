ec8e7984bdc345e63ba0f6e224d03e04a68f1da2
Gawin Guo <69577009+283Gawin@users.noreply.github.com>
2026-08-22 12:18:35 +0800

    Add secure keychain storage for Pixiv tokens

---
+++ native-ios/NativePixEz/Services/KeychainStore.swift
@@ -0,0 +1,57 @@
+import Foundation
+import Security
+
+struct KeychainStore {
+    private let service: String
+
+    init(service: String = "com.gawin283.pixez.native") {
+        self.service = service
+    }
+
+    func save<T: Codable>(_ value: T, account: String) throws {
+        let data = try JSONEncoder().encode(value)
+        delete(account: account)
+
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrService as String: service,
+            kSecAttrAccount as String: account,
+            kSecValueData as String: data,
+            kSecAttrAccessible as String: kSecAttrAccessibleAfterFirstUnlockThisDeviceOnly
+        ]
+        let status = SecItemAdd(query as CFDictionary, nil)
+        guard status == errSecSuccess else {
+            throw KeychainError.unhandled(status)
+        }
+    }
+
+    func load<T: Codable>(_ type: T.Type, account: String) -> T? {
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrService as String: service,
+            kSecAttrAccount as String: account,
+            kSecReturnData as String: true,
+            kSecMatchLimit as String: kSecMatchLimitOne
+        ]
+
+        var item: CFTypeRef?
+        let status = SecItemCopyMatching(query as CFDictionary, &item)
+        guard status == errSecSuccess, let data = item as? Data else {
+            return nil
+        }
+        return try? JSONDecoder().decode(type, from: data)
+    }
+
+    func delete(account: String) {
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrService as String: service,
+            kSecAttrAccount as String: account
+        ]
+        SecItemDelete(query as CFDictionary)
+    }
+}
+
+enum KeychainError: Error {
+    case unhandled(OSStatus)
+}
